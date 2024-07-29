cf0daa74c58df8c4d4a05b2d51ac2f0a3d5f19d2
epruebaupb <epruebaupb@gmail.com>
2024-07-29 18:58:27 -0500

    cambio en el código

--- app/src/main/java/full_talleres/Taller1.java
+++ app/src/main/java/full_talleres/Taller1.java
@@ -1,18 +1,3 @@
-/*
- * Este es el primer taller de lógica de programación. Tenga en cuenta la siguiente rúbrica que debe ser  
- * tenida en cuenta para cada ejercicio y se basa en los mandamientos del programador. El taller es una 
- * herramienta para comenzar a preparar la evaluación donde los descuentos si van a ser tenidos en cuenta. 
- * Con este taller, usted irá aprendiendo a aplicar buenas prácticas en la programación.
- * 
- * Al valor que obtenga en la evaluación automática de github, se revisará si cumple con buenas prácticas
- * de acuerdo con la siguiente rúbrica que se descuenta en cada ejercicio y es acumulativa:
- * Descuento por quemar valores en el código y no usar constantes: -0.5
- * Descuento por no implementar control de errores con trycatch: -0.8
- * Descuento por no escribir el código ordenado, identado: -0.4
- * 
- * RÉTESE, PÓNGASE A PRUEBA!!!
- * 
- */
 package full_talleres;
 
 public class Taller1 {
@@ -29,11 +14,6 @@ public class Taller1 {
         System.out.println(Calcular_peso_luna((byte)80));
     }
 
-    /* 
-     2.	Diseñe un algoritmo e implemente la función Convertir_km_seg que: reciba una velocidad entera en Km/seg 
-     y la convierta a metros/seg y a metros/hora. Retorne un string del tipo: ### m/s - ### m/h. Si hay error, 
-     devuelva el string "Error en la conversion"
-    */
     
     public static String Convertir_km_seg(int vel_km_seg){
 
@@ -56,9 +36,6 @@ public class Taller1 {
 
     }
 
-    /*3. Diseñe un algoritmo e implemente la función Convertir_cm_lt que: reciba una cantidad double expresada en 
-    cc (centímetros cúbicos) y devuelva un float con su equivalente en litros. Si hay algún error, retorne 0.
-    */
   
     public static float Convertir_cm_lt (double cant_cc){
 
@@ -75,10 +52,6 @@ public class Taller1 {
     }
 
   
-   /*4.	Diseñe un algoritmo e implemente la función Convertir_us_cops que reciba una cantidad entera de dólares 
-   y devuelva su equivalente en pesos usando una TRM de $4170 pesos por cada dólar. La función recibe enteros
-   y devuelve enteros, pueden ser grandes. Si hay algún error, retorne -1.
-   */
    
     public static long Convertir_us_cops(long monto_us){
     try {
@@ -92,10 +65,6 @@ public class Taller1 {
    }
   
    
-   /*5.	Diseñe un algoritmo e implemente la función Convertir_cent_far que recibe la temperatura real en grados 
-   centígrados y la devuelve en grados Fahrenheit (averiguar la fórmula) F = 32 + ( 9 * C / 5). Si hay algún error, 
-   retorne 0.
-   */
 
     public static float Convertir_cent_far(float temp_grad_cent){
 
@@ -112,10 +81,6 @@ public class Taller1 {
     }        
 
    
-   /*6.	Diseñe un algoritmo e implemente la función Calcular_segs que recibirá el número de Días, el número de horas, 
-   el número de minutos y número segundos como enteros bytes, positivos y devuelva todo en segundos en un entero. 
-   Si hay algún error, devuelva -1.
-   */
 
    public static int Calcular_segs(short dd, short hh, short mm, short ss){
         try {
@@ -134,11 +99,6 @@ public class Taller1 {
         }
     }
 
-   /*7.	Un usuario tiene un sistema de báscula para pesar camiones. Diseñe un algoritmo e implemente la función 
-   Calcular_peso_carga que reciba un float con el peso total del camión cargado en toneladas y otro float con 
-   lo que pesa el camión vacío en toneladas, y devuelva el peso neto de la carga en kilos y toneladas 
-   en un string del tipo: "### kilos - ### toneladas". 
-   Si hay algún error, devuelva en un string "Error en la función Calcular_peso_carga"*/
 
    public static String Calcular_peso_carga(float peso_cargado, float peso_vacio) {
         try {
@@ -157,11 +117,6 @@ public class Taller1 {
         }
    }
 
-   /*8.	Diseñe un algoritmo e implemente la función Calcular_horasxviaje que calcule y devuelva un float con las horas  
-   necesarias para alcanzar un destino que es recibido en un string, además de un short con la distancia en kms
-   otro dato short que es la velocidad  promedio que alcanzará también el vehículo en kilómetros/hora.  
-   Si hay algún error, devuelva -1.
-   */
 
    public static float Calcular_horasxviaje (String destino, short distancia_km, short velocidad_kmxh){
 
@@ -177,15 +132,6 @@ public class Taller1 {
 
    }    
    
-   /*9.	Un avión necesita cargar combustible para cubrir sus rutas programadas en el día. 
-   Cada 0.2 toneladas de combustible puede recorrer 60.8 Km en velocidad de crucero. 
-   En el despegue el avión consume 1.2 toneladas de combustible y en el aterrizaje consume 0.4 toneladas. 
-   
-   El piloto desea que sea diseñado un algoritmo e implementado en la función Calcular_combustible, 
-   el kilometraje total para cada una de cuatro rutas que son datos enteros, y devuelva un float con la cantidad total 
-   de combustible que debe ser cargado en el avión.
-   
-   Si hay algún error, devuelva -1.*/
 
    public static float Calcular_combustible(int km_r1, int km_r2, int km_r3, int km_r4){
 
@@ -209,13 +155,6 @@ public class Taller1 {
    }
    
    
-   /*10. Diseñe un algoritmo e implemente la función Calcular_peso_luna que recibe un byte con el peso en la tierra en kilos
-   y devuelve un double el equivalente de ese peso en la luna en Newtons. Utilice las siguientes fórmulas.
-   peso_tierra_new = peso_kilos * 9.81m/s2
-   peso_luna_new = peso_tierra_new * 0.165
-   
-   Si hay algún error, devuelva 0.
-   */
    
    public static double Calcular_peso_luna(byte peso_kl){
 
