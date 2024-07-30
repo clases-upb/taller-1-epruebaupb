669d65d20388923d0db78d25f372bd73d99e2917
epruebaupb <epruebaupb@gmail.com>
2024-07-29 19:06:16 -0500

    correccion 63.8

--- app/src/test/java/full_talleres/AppTest.java
+++ app/src/test/java/full_talleres/AppTest.java
@@ -55,7 +55,7 @@ class AppTest {
     @Test void t1_calcular_comustible(){
         Taller1 classUnderTest = new Taller1();
         assertEquals(14.337502, classUnderTest.Calcular_combustible(638, 312, 1243, 220), 
-        "El total de combustible en tonealdas esperado es: 14.337502 para los kms de las siguiente cuatro rutas: 63.8, 312, 1243, 220");
+        "El total de combustible en tonealdas esperado es: 14.337502 para los kms de las siguiente cuatro rutas: 638, 312, 1243, 220");
     }
 
     @Test void t1_calcular_peso_luna(){
